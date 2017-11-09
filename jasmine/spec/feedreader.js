23aed61b46c146b9e1cc81679cbe25cb75791bbc
iiffd <sml.hng@gmail.com>
2017-11-09 10:43:41 -0800

    Add test to check menu click shows and hides menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -64,16 +64,23 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('menu shows and hides on click', function() {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+          });
         });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+       });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
